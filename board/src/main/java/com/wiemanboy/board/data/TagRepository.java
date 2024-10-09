335c268be0d9dab98dbac52e28a92456375f2247
wiemanboy <wiemanboy@gmail.com>
2024-10-09 11:22:37 +0200

    ADD: repositories

---
+++ board/src/main/java/com/wiemanboy/board/data/TagRepository.java
@@ -0,0 +1,11 @@
+package com.wiemanboy.board.data;
+
+import com.wiemanboy.board.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+}
