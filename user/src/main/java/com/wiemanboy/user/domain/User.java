190098254887c3c934635a00c726bd9bef825abe
LegendWolf-Dev <jochem.nieuwenhuizen@student.hu.nl>
2024-10-09 12:14:45 +0200

    ADD: Integrationtests made for UserService

--- user/src/main/java/com/wiemanboy/user/domain/User.java
+++ user/src/main/java/com/wiemanboy/user/domain/User.java
@@ -3,28 +3,28 @@ package com.wiemanboy.user.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 public class User {
-    @Id
-    @GeneratedValue(generator = "increment")
+    @Id()
+    @GeneratedValue(generator = "UUID")
     private UUID id;
 
     private String username;
 
     public User(String username) {
         this.username = username;
-    }
-
-    public User() {
-
     }
 }
