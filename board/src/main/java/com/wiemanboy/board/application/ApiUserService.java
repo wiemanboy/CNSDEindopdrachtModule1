0631fb3acbd1a19388d853107d1598c8a051bf93
LegendWolf-Dev <jochem.nieuwenhuizen@student.hu.nl>
2024-10-10 12:46:37 +0200

    CHANGE: Renamed UserServiceImplementation.java to ApiUserService.java.

--- board/src/main/java/com/wiemanboy/board/application/ApiUserService.java
+++ board/src/main/java/com/wiemanboy/board/application/ApiUserService.java
@@ -7,18 +7,18 @@ import org.springframework.web.client.RestTemplate;
 import java.util.UUID;
 
 @Service
-public class UserServiceImplementation implements UserService {
+public class ApiUserService implements UserService {
     @Value("${user.baseURL}")
     private String url;
 
     private final RestTemplate restTemplate;
 
-    public UserServiceImplementation(RestTemplate restTemplate) {
+    public ApiUserService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
     public Boolean validateUser(UUID id) {
-        return restTemplate.getForObject(String.format("%s%s/exists", url, id) , Boolean.class);
+        return restTemplate.getForObject(String.format("%s/%s/exists", url, id) , Boolean.class);
     }
 }
