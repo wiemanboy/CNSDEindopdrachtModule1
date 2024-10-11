4ef4a76bfc585a6d8760a268d2ca759cec923980
wiemanboy <wiemanboy@gmail.com>
2024-10-11 11:33:07 +0200

    ADD: fetch board data

---
+++ frontend/src/routes/board/+page.svelte
@@ -0,0 +1,47 @@
+<!--
+Board Page
+
+-->
+
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+	import { container } from "$lib/index";
+	import type BoardRepository from "$lib/data/board/BoardRepository";
+	import types from "$lib/types";
+	import type BoardDto from "$lib/dtos/board/BoardDto";
+
+	const boardRepository = container.get<BoardRepository>(types.boardRepository);
+
+	let error: string;
+	let id: string;
+	let board: BoardDto;
+
+	onMount(() => {
+		const searchParams = new URLSearchParams(window.location.search);
+		id = searchParams.get("id") || "";
+		if (!id || id === "") {
+			goto("/boards");
+			return;
+		}
+		boardRepository.getBoard(id)
+			.then((data) => {
+				board = data;
+			})
+			.catch((err) => {
+				error = err;
+			});
+	});
+</script>
+
+
+<div>
+	{#if (error !== undefined)}
+		<p>Error while fetching board</p>
+	{:else if (board === undefined)}
+		<p>Loading...</p>
+	{:else}
+		<h1>{board.title}</h1>
+	{/if}
+</div>
+
