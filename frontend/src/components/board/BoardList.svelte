f8950ddc2a8834320d1f737e187fb8a2beeb8398
LegendWolf-Dev <jochem.nieuwenhuizen@student.hu.nl>
2024-10-11 11:12:03 +0200

    ADD: BoardList.svelte

--- frontend/src/components/board/BoardList.svelte
+++ frontend/src/components/board/BoardList.svelte
@@ -4,9 +4,30 @@ BoardList
 -->
 
 <script lang="ts">
+    import {container} from '$lib/index';
+    import types from '$lib/types';
+    import type BoardRepository from '$lib/data/board/BoardRepository';
+    import type BoardDto from '$lib/dtos/board/BoardDto';
+
+    const boardsRepository: BoardRepository = container.get(types.boardRepository)
+
+    const boards:Promise<BoardDto[]> = boardsRepository.getBoards()
 
 </script>
 
 <div>
-
+    <h1>Board List</h1>
+    {#await boards}
+        <p>Waiting for boards</p>
+    {:then boardlist}
+        <ul>
+            {#each boardlist as board}
+                <li>
+                    <a href="/board?id={board.id}">{board.title}</a>
+                </li>
+            {/each}
+        </ul>
+    {:catch error}
+        <p>{error.message}</p>
+    {/await}
 </div>
